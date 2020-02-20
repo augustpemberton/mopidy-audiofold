66d8ae8c430444eab4fa16fbf68a601b8d86c461
August Pemberton <augustmpemberton@gmail.com>
2020-02-20 09:46:57 +0000

    Move dist

--- mopidy_audiofold/__init__.py
+++ mopidy_audiofold/__init__.py
@@ -1,5 +1,6 @@
 import logging
 import pathlib
+import os
 
 import pkg_resources
 
@@ -10,40 +11,21 @@ __version__ = pkg_resources.get_distribution("Mopidy-Audiofold").version
 # TODO: If you need to log, use loggers named after the current Python module
 logger = logging.getLogger(__name__)
 
-
 class Extension(ext.Extension):
 
-    dist_name = "Mopidy-Audiofold"
-    ext_name = "audiofold"
+    dist_name = 'Mopidy-Audiofold'
+    ext_name = 'audiofold'
     version = __version__
 
-    def get_default_config(self):
-        return config.read(pathlib.Path(__file__).parent / "ext.conf")
-
     def get_config_schema(self):
-        schema = super().get_config_schema()
-        # TODO: Comment in and edit, or remove entirely
-        #schema["username"] = config.String()
-        #schema["password"] = config.Secret()
+        schema = super(Extension, self).get_config_schema()
         return schema
 
+    def get_default_config(self):
+        return config.read(os.path.join(os.path.dirname(__file__), 'ext.conf'))
+
     def setup(self, registry):
-        # You will typically only implement one of the following things
-        # in a single extension.
-
-        # TODO: Edit or remove entirely
-        from .frontend import FoobarFrontend
-        registry.add("frontend", FoobarFrontend)
-
-        # TODO: Edit or remove entirely
-        from .backend import FoobarBackend
-        registry.add("backend", FoobarBackend)
-
-        # TODO: Edit or remove entirely
-        registry.add(
-            "http:static",
-            {
-                "name": self.ext_name,
-                "path": str(pathlib.Path(__file__).parent / "static"),
-            },
-        )
+        registry.add('http:static', {
+            'name': self.ext_name,
+            'path': os.path.join(os.path.dirname(__file__), 'static')
+        })
